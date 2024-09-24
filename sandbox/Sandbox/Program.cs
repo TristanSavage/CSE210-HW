778ada81839eb20b2da4552169acf083fd8d2807
Tristan Maughan <tristanmaughan@Tristans-iMac.local>
2024-09-24 16:43:37 -0600

    Finished Prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+
+        int x;
+        x = 10;
+        Console.WriteLine(x);
+
+        float y = 5;
+        Console.WriteLine(y);
+
+        Console.Write("There will not be a newline after this.");
+
+        Console.WriteLine("Hello World!");
+
+        // Console.Write("What is your favorite color? ");
+        // string color = Console.ReadLine();
+
+        if (x > y)
+            {
+            Console.WriteLine("greater");
+            }
+
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}.");
+        
     }
 }
