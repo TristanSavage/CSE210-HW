894f02a235f7b452243386232b345260f44e2dcf
Tristan Maughan <tristanmaughan@Tristans-iMac.local>
2024-09-25 22:02:47 -0600

    Finished Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // returnType FunctionName(dataType parameter1, dataType parameter2)
+    //     {
+    //         function_body
+    //     }
+
+            static void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to the Program!");
+            }
+
+            static string PromptUserName()
+            {
+                Console.Write("Please enter your name: ");
+                string name = Console.ReadLine();
+                
+                return name;
+            }
+
+            static int PromptUserNumber()
+            {
+                Console.Write("Please enter your fravorite number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                return number;
+            }
+
+            static int SquareNumber(int number)
+            {
+                return number*number;
+            }
+
+            static void DisplayResults(string name, int number)
+            {
+                Console.WriteLine($"{name}, the square of your favorite number is {number}.");
+            }
+
+            DisplayWelcome();
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+            number = SquareNumber(number);
+            DisplayResults(name,number);
+
     }
 }
