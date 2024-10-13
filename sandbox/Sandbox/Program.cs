411b0f8c26a4e167353aa53e3840e672fe8d74e9
Tristan Maughan <tristanmaughan@Tristans-iMac.local>
2024-10-12 23:55:10 -0600

    Finished Develope 02

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,54 +1,86 @@
 using System;
+using System.Runtime.InteropServices;
+
+class Box
+{
+    public double _height = 0;
+    public double _width = 0;
+    public double _length = 0;
+
+    public string _unit = "";
+
+    public string Describe()
+    {
+        return $"Box + {_length} + {_width} + {_height} {_unit}";
+    }
+
+    public double Volume()
+    {
+        return _length * _width * _height;
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        int x;
-        x = 10;
-        Console.WriteLine(x);
+        Box small = new Box();
+        small._length = 10;
+        small._width = 7;
+        small._height = 5;
+        small._unit = "in";
 
-        float y = 5;
-        Console.WriteLine(y);
+        Console.WriteLine(small.Describe());
 
-        Console.Write("There will not be a newline after this.");
+        Console.WriteLine(small.Volume());
 
-        Console.WriteLine("Hello World!");
+        // int x;
+        // x = 10;
+        // Console.WriteLine(x);
 
-        // Console.Write("What is your favorite color? ");
-        // string color = Console.ReadLine();
+        // float y = 5;
+        // Console.WriteLine(y);
 
-        string school = "BYU-Idaho";
-        Console.WriteLine($"I am studying at {school}.");
+        // Console.Write("There will not be a newline after this.");
 
-        if (x > y)
-            {
-            Console.WriteLine("greater than");
-            }
+        // Console.WriteLine("Hello World!");
+
+        // // Console.Write("What is your favorite color? ");
+        // // string color = Console.ReadLine();
+
+        // string school = "BYU-Idaho";
+        // Console.WriteLine($"I am studying at {school}.");
+
+        // if (x > y)
+        //     {
+        //     Console.WriteLine("greater than");
+        //     }
         
-        if (x > y)
-            {
-                Console.WriteLine("greater than");
-            }
-            else
-            {
-                Console.WriteLine("less than");
-            }
+        // if (x > y)
+        //     {
+        //         Console.WriteLine("greater than");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("less than");
+        //     }
 
-        if (x > y)
-            {
-                Console.WriteLine("greater than y");
-            }
-            else if (x > z)
-            {
-                Console.WriteLine("greater than z");
-            }
-            else
-            {
-                Console.WriteLine("less than both");
-            }
+        // if (x > y)
+        //     {
+        //         Console.WriteLine("greater than y");
+        //     }
+        //     else if (x > z)
+        //     {
+        //         Console.WriteLine("greater than z");
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine("less than both");
+        //     }
+
         
+
 
     }
 }
