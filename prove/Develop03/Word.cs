382295f4066a4fd4a9dda16845820411386cd8ac
TristanSavage <mau21004@byui.edu>
2024-10-27 23:55:07 -0600

    Update Word.cs

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,1 +1,46 @@
 
+class Word
+{
+    // Attributes
+    private string _word;
+    private bool _hidden = false;
+
+    // Constructors
+    public Word(string word, bool hidden)
+    {
+        _word = word;
+        _hidden = hidden;
+    }
+
+    // Behaviors
+    // public void SetWord(string word)
+    // {
+    //     _word = word;
+    // }
+
+    public string GetWord()
+    {
+        return _word;
+    }
+
+    public string GetHidden()
+    {
+        return _hidden.ToString();
+    }
+
+    public void HideWord()
+    {
+        _hidden = true;
+
+        char[] letters = _word.ToCharArray();
+        List<char> chars = new List<char>();
+        
+        foreach (char letter in letters)
+        {
+            chars.Add('_');
+        }
+
+        _word = String.Concat(chars);
+    }
+
+}
